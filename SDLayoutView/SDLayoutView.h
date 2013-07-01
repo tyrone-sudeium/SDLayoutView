bb04d500922382b0c497be5bc648f63d28489945
Tyrone Trevorrow <tyrone@sudeium.com>
2013-07-01 21:44:35 +1000

    Initial Commit

---
+++ SDLayoutView/SDLayoutView.h
@@ -0,0 +1,13 @@
+//
+//  SDLayoutView.h
+//  SDLayoutView
+//
+//  Created by Tyrone Trevorrow on 1/07/13.
+//  Copyright (c) 2013 Sudeium. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDLayoutView : NSObject
+
+@end
---
+++ SDLayoutView/SDLayoutView.m
@@ -0,0 +1,13 @@
+//
+//  SDLayoutView.m
+//  SDLayoutView
+//
+//  Created by Tyrone Trevorrow on 1/07/13.
+//  Copyright (c) 2013 Sudeium. All rights reserved.
+//
+
+#import "SDLayoutView.h"
+
+@implementation SDLayoutView
+
+@end
